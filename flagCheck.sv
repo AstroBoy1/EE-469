8d6a3c5c16933fa1c4d7a9ad57d99e9818f2a704
Michael Omori <omorim@uw.edu>
2016-10-20 13:49:34 -0700

    added testbench

--- flagCheck.sv
+++ flagCheck.sv
@@ -1,33 +1,29 @@
 // this module checks the ouput of the alu for
 // zero, overflow, carryout, and negative
 
-// TODO: assign, equals, and with 1, initial begin, always_comb?
-
 module flagCheck(bits, zero, over, cOut, neg, cout64, cin64);
 	input [63:0] bits; // the output bits from an alu operation
 	input cin64, cout64; // carry out from 64th bit
 	output zero, over, cOut, neg; // zero, overflow, carryout, negative
-	wire value; // value used in noring each bit for checking if zero
+	wire [63:0] value; // value used in noring each bit for checking if zero
 	
-	// true if every bit is zero
-	assign value = bits[0];
-	
+	// zero check: true if every bit is zero
+	and andValue(value[0], bits[0], 1);
 	genvar i;
 	generate
-		for(i=1; i<=63; i++) begin: eachPair
-			nor bitNor(value, value, bits[i]);
+		for(i=1; i<=63; i++) begin : eachPair
+			nor bitNor(value[i], value[i-1], bits[i]);
 		end
 	endgenerate
+	assign zero = value[63];
 	
-	assign zero = value;
-	
-	// xor of carry in and carry out of 64th bit
+	// over flow check: xor of carry in and carry out of 64th bit
 	xor cinXcout(over, cin64, cout64);
 	
-	// true if their is a carry out from the 64th bit
+	// carryout check: true if their is a carry out from the 64th bit
 	assign cOut = cout64;
 	
-	// true if the 64th bit is a 1
+	// negative check: true if the 64th bit is a 1
 	assign neg = bits[63];
 endmodule
 
@@ -41,8 +37,11 @@ module flagCheck_testbench();
 	
 	integer i;
 	initial begin
-		for (i=0; i<2**66; i++) begin
-			{cin64, cout64, bits} = i; #10;
+		for (i=0; i<2**2; i++) begin
+			{cin64, cout64} = i; #10;
+			bits = 64'h0000000000000000; #10; // bits all zero
+			bits = 64'hffffffffffffffff; #10; // bits all 1
+			bits = i * 64'hffbf1fffff2fff0f; #10; // some random bits
 		end
 	end
 endmodule
